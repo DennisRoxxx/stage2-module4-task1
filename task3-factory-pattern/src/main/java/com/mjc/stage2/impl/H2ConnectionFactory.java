c324b7c7e8d532e890860b452938b54d9410cee3
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2023-02-16 15:10:41 +0300

    done

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,25 @@
 package com.mjc.stage2.impl;
+import com.mjc.stage2.ConnectionFactory;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection(){
+        Connection connection = null;
+        Properties properties = new Properties();
+        try {
+            properties.load(H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties"));
+            connection = DriverManager.getConnection(
+                    properties.getProperty("db_url"),
+                    properties.getProperty("user"),
+                    properties.getProperty("password"));
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+        return connection;
+    }
 }
 
